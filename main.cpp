61cd3cd39c025d30b2a7656d0e7933008e633c86
gudtn2 <tyrueu@gmail.com>
2024-06-28 04:35:27 +0900

    0628
    2번문제 자연수의 합 문제풀이

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
+using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+/* 1¹ø 
 int main() {
 	int n, m, i, sum = 0;
 	std::cin>>n>>m;
@@ -14,3 +15,25 @@ int main() {
 	std::cout<<sum;
 	return 0;
 }
+*/
+
+/* 2¹ø 
+int main() {
+	int a, b, sum = 0;
+	cin>>a>>b;
+	for(int i = a; i<b; i++)
+	{
+		cout << i << " + ";
+		sum += i;
+	}
+	sum += b;
+	cout << b << " = " << sum;
+	return 0;
+}
+*/
+
+int main(){
+	
+	return 0;	
+}
+
