c94b6530c268911e985985e21fb462eedf61809e
gudtn2 <tyrueu@gmail.com>
2024-06-29 02:37:29 +0900

    0629
    3번 진약수의 합

--- main.cpp
+++ main.cpp
@@ -32,8 +32,25 @@ int main() {
 }
 */
 
+/* 3¹ø 
+int main(){
+	int n, sum = 1;
+	cin >> n;
+	cout << 1;
+	for(int i = 2; i < n; i++)
+	{
+		if(n%i==0)
+		{
+			cout << " + " << i;
+			sum += i;
+		}
+	}
+	cout << " = " << sum;
+	return 0;	
+}
+*/
+
 int main(){
 	
 	return 0;	
 }
-
