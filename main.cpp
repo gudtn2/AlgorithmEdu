b85c389eec83981dbb3a67a7c3c9ae000351ecdb
gudtn2 <tyrueu@gmail.com>
2024-07-03 02:55:59 +0900

    0703
    문제5번 나이차이계산

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -50,6 +50,7 @@ int main(){
 }
 */
 
+/* 4¹ø 
 int main(){
 	//freopen("input.txt", "rt", stdin);
 	int n, a, max = INT_MIN, min = INT_MAX;
@@ -67,3 +68,19 @@ int main(){
 	cout << max - min;
 	return 0;	
 }
+*/
+
+int main(){
+	//freopen("input.txt", "rt", stdin);
+	char a[20];
+	int year, age;
+	scanf("%s", &a);
+	if(a[7] == '1' || a[7] == '2') year = 1900 + ((a[0] - 48) * 10 + (a[1] - 48));
+	else year = 2000 + ((a[0] - 48) * 10 + (a[1] -48));
+	age = 2019 - year + 1;
+	printf("%d ", age);
+	if(a[7] == '1' || a[7] == '3') printf("M\n");
+	else printf("W\n");
+	
+	return 0;
+ } 
