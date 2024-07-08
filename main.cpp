4a81549fcfd6e0a38fd3dfb728b2c9e61cf1515f
gudtn2 <tyrueu@gmail.com>
2024-07-09 04:07:26 +0900

    0709
    8번 올바른 괄호

--- main.cpp
+++ main.cpp
@@ -108,6 +108,7 @@ int main(){
  }  
  */
  
+ /* 7¹ø 
   int main(){
  	freopen("input.txt", "rt", stdin);
  	char a[101], b[101];
@@ -124,4 +125,21 @@ int main(){
 	 b[p] = '\0';
 	 printf("%s\n", b);
  	return 0;
- } 
+ }  
+ */
+ 
+ int main(){
+ 	freopen("input.txt", "rt", stdin);
+ 	char a[100];
+ 	int i, cnt = 0;
+ 	scanf("%s", &a);
+ 	for(i =0; a[i] != '\0'; i++){
+ 		if(a[i] == '(') cnt++;
+ 		else if(a[i] == ')') cnt--;
+ 		if(cnt < 0) break;
+	}
+	if(cnt == 0) printf("YES\n");
+	else printf("NO\n");
+ 	
+ 	return 0;
+ }
