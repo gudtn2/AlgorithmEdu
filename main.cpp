afe502aceaebfd24c55970b125fcaacf41bd1d62
gudtn2 <tyrueu@gmail.com>
2024-07-16 03:26:54 +0900

    0716
    12번 모든 숫자의 합(Large)

--- main.cpp
+++ main.cpp
@@ -195,6 +195,7 @@ int main(){
  } 
  */
  
+ /* 11¹ø 
  int main(){
  	int n, tmp, sum = 0;
  	scanf("%d", &n);
@@ -209,3 +210,19 @@ int main(){
  	
  	return 0;
  } 
+ */
+ 
+ int main(){
+ 	int n, sum = 0, c = 1, d = 9, res = 0;
+ 	scanf("%d", &n);
+ 	while(sum + d < n){
+ 		res = res+(c * d);
+ 		sum = sum + d;
+ 		c++;
+ 		d = d * 10;
+	 }
+	 res = res + ((n-sum) * c);
+	 printf("%d\n", res);
+ 	
+ 	return 0;
+ }
