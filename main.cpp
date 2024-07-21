18e53dcfa20439acc68f5cc8cb1ec478dc23b3d9
gudtn2 <tyrueu@gmail.com>
2024-07-22 03:18:10 +0900

    0722
    14번 뒤집은 소수

--- main.cpp
+++ main.cpp
@@ -229,6 +229,7 @@ int main(){
  }
  */
  
+ /* 13¹ø 
  int ch[10];
  int main(){
  	int digit, max = -2147000000, res;
@@ -245,6 +246,41 @@ int main(){
 		}
 	}
 	printf("%d\n", res);
+ 	
+ 	return 0;
+ }
+ */
+ 
+ int reverse(int x){
+ 	int res = 0, tmp;
+ 	while(x > 0){
+ 		tmp = x % 10;
+ 		res = res * 10 + tmp;
+ 		x = x / 10;
+	 }
+	 return res;
+ }
+ 
+ bool isPrime(int x){
+ 	if(x == 1) return false;
+ 	bool flag = true;
+ 	for(int i = 2; i < x; i++){
+ 		if(x % i == 0){
+ 			flag = false;
+ 			break;
+		 }
+	 }
+	 return flag;
+ }
+ 
+ int main(){ 	
+ 	int n, num, tmp;
+ 	scanf("%d", &n);
+ 	for(int i = 1; i <= n; i++){
+ 		scanf("%d", &num);
+ 		tmp = reverse(num);
+ 		if(isPrime(tmp)) printf("%d ", tmp);
+	 }
  	
  	return 0;
  }
