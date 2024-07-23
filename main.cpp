508881cf75c7de99ceafe7e679ef0b1329da6a42
gudtn2 <tyrueu@gmail.com>
2024-07-24 03:34:13 +0900

    0724
    15번 소수의 개수

--- main.cpp
+++ main.cpp
@@ -251,6 +251,7 @@ int main(){
  }
  */
  
+ /* 14번 
  int reverse(int x){
  	int res = 0, tmp;
  	while(x > 0){
@@ -284,3 +285,22 @@ int main(){
  	
  	return 0;
  }
+ */
+ 
+ int main(){
+ 	int n, i, j, flag, cnt = 0;
+ 	scanf("%d", &n);
+ 	for(i = 2; i <= n; i++){
+ 		flag = 1;
+ 		for(j = 2; j * j <= i; j++){  //j * j로 제곱을 해준다 
+ 			if(i % j == 0){ 
+ 				flag = 0;
+ 				break;
+			 }
+		 }
+		 if(flag == 1) cnt++;
+	 }
+	 printf("%d\n", cnt);
+ 	
+ 	return 0;
+ } 
